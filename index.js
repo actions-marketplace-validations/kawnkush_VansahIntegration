b74a149d58a49bff3e053d3b3fd6c136b2b75d24
Shubham Mourya <95007067+ShubhamMoury@users.noreply.github.com>
2022-08-16 23:40:32 +0530

    Testing

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const core = require('@actions/core'); //ES5
 
 
-const sourceFilePath = core.getInput('path');
+const vansahToken = core.getInput('path');
+const testFormat = core.getInput('testFormat');
+const testPaths = core.getInput('testPaths');
+const projectKeyonJira = core.getInput('projectKeyonJira');
+const case_key = core.getInput('case_key');
+const log_identifier = core.getInput('log_identifier');
+const comment = core.getInput('comment');
 
-console.log(sourceFilePath);
+console.log(vansahToken);
+console.log(testFormat);
+console.log(testPaths);
+console.log(projectKeyonJira);
+console.log(case_key);
+console.log(log_identifier);
+console.log(comment);
